afee37d39dbf333f7ba27290e5068d5ec2375914
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-01-11 17:25:47 -0500

    GET route established, tested with Postman

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,6 +3,20 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
+router.get('/', (req, res) => {
+    const dbQuery = `SELECT * FROM "todo" ORDER BY "priority" DESC`
+    pool
+    .query((dbQuery))
+    .then((result) => {
+        console.log('Successfully queried DB: ', result);
+        res.send(result.rows);
+    })
+    .catch((error) => {
+        console.error("Can't retrieve data: ", error);
+        res.sendStatus(500)
+    })
+})
+
 
 // POST
 
