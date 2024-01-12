96e75e43bf61794f14913c3492eef3cea42e7db7
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-01-11 19:14:24 -0500

    Built useEffect to make initial GET call for template todo data

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,22 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import { getToDoList } from '../../todoApi/todoApi'
 
 function App () {
+  const [toDoList, setToDoList] = useState();
   
+  useEffect(() => {
+    console.log('useEffect start');
+
+    const todoCall = getToDoList();
+    todoCall
+    .then((response) => {
+      setToDoList(response.data)
+    })
+    .catch((error) => {
+      console.error(error);
+      resizeBy.sendStatus(500);
+    })
+  })
   return (
     <div>
       <h1>TO DO APP</h1>
