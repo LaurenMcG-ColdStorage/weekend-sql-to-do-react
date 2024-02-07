b51f954ddb1958bfbec94a9b2df2f691269ae461
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-02-06 20:36:26 -0500

    Updated project with env config

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore, combineReducers, applyMiddleware }from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 import './index.css';
 import App from './components/App/App';
 
